f0988b273cacf0c39c8f2d766f8a5c613ac548c0
RaunakDey <39820997+RaunakDey@users.noreply.github.com>
2021-06-07 15:40:15 +0530

    Add files via upload

---
+++ OU_a.m
@@ -0,0 +1,61 @@
+clc;
+clear all;
+
+%%%%% parameter defined %%%%%
+n=100e4; %number of data points
+r=randn(1,n);
+r2=randn(1,n);
+to=0.0025;
+A=0.1*(0.6e-6)^2;
+
+
+
+%%% parameters %%%
+eta=89e-5;
+a=1.5e-6;
+gamma=6*pi*eta*a;
+fc=130;
+td=1/(2*pi*fc);
+kb=1.38e-23;
+T=300;
+D=kb*T/gamma;
+k=2*pi*fc*gamma;
+
+dt=0.0001;
+
+
+%OU noise generated
+lambda=zeros(1,n);
+for i=2:n
+    lambda(i)=lambda(i-1)+dt*(-lambda(i-1)/to)+sqrt(2*A*dt)*r(i-1);
+end
+
+%position simulated
+x_pos=zeros(1,n);
+time=zeros(1,n);
+x_pos(1)=0;
+for i=2:n
+   x_pos(i)=x_pos(i-1)+dt*(-(x_pos(i-1)-lambda(i-1))/td)+sqrt(2*D*dt)*r2(i-1); 
+   time(i)=time(i-1)+dt;
+end
+
+%%
+
+data=x_pos-mean(x_pos);
+Fs = 1/dt;
+
+for l=1:1:200
+k=12*pi^2*eta*1.5e-6*l;%sqrt(608);
+for i=1:n-1
+noise(i)=-6*pi*eta*1.5e-6*(data(i+1)-data(i))*Fs-k*(data(i)-lambda(i));
+end
+noise(n)=0;
+noise=(noise-mean(noise))./std(noise);
+auto1=xcorr(noise,noise);
+auto1=(auto1-min(auto1))./(max(auto1)-min(auto1));
+value(l)=auto1(n-2)-auto1(1);
+l
+end 
+
+plot(1:200,value);grid on;
+
